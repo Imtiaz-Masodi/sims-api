5b5a9f06485f733ef22a7b74d09014249c75b2a4
Mohd Imtiaz <mohdimtiaz.uddin@highspot.com>
2024-10-13 13:29:49 +0530

    Initial Commit

---
+++ src/test/java/app/soft_tenders/sims/StudentInformationManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package app.soft_tenders.sims;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentInformationManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
